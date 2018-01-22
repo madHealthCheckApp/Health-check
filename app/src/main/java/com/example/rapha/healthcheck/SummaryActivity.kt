03b7563e687f3477d971ab0b2393cbfca66ccb50
Rapha <raphaelmueller1@hotmail.com>
2018-01-22 14:55:16 +0100

    Initialization

---
+++ app/src/main/java/com/example/rapha/healthcheck/SummaryActivity.kt
@@ -0,0 +1,15 @@
+package com.example.rapha.healthcheck
+
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+
+
+class SummaryActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_summary)
+    }
+}
